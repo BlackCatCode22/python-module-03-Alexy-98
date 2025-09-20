df8786225edda4ab6980f2414f5be00be240fcfb
Alexy-98 <AlexyBarajas98@gmail.com>
2025-09-19 23:04:44 -0700

    Add files via upload

---
+++ min_and_max_numbers.py
@@ -0,0 +1,19 @@
+#Initialize an empty list to store numbers7
+numbers = []
+
+while True:
+    user_input = input("Enter a number or 'done' to finish: ")
+    if user_input == 'done':
+        break
+
+    try:
+        number = float(user_input)
+        numbers.append(number)
+    except ValueError:
+        print("Invalid input. Please enter a numberic value or 'done' to finish.")
+
+if numbers:
+    print("Maximum: ", max(numbers))
+    print("Minimum: ", min(numbers))
+else:
+    print("No numbers were entered. Please try again.")
