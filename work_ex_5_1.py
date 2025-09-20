fa25a5ceea677b1e41b3d3693c2fc674f54b12bb
Alexy-98 <AlexyBarajas98@gmail.com>
2025-09-19 21:57:43 -0700

    Add files via upload

---
+++ work_ex_5_1.py
@@ -0,0 +1,19 @@
+#Repeated Integers with Total, Count, and Average
+num = 0
+tot = 0.0
+while True:
+    sval = input("Enter a number: ")
+    if sval == 'done':
+        break
+    try:
+        fval = float(sval)
+    except:
+        print("Invalid input")
+        continue
+    fval = float(sval)
+    #print(fval)
+    num = num + 1
+    tot = tot + fval
+
+#print("All done!")
+print(tot,num,tot/num)
